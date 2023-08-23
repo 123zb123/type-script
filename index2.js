8387bf1a858fd3ffa8e6c359897c4b9fa9f8670e
Zvika Berkovich <zvika0556705015@gmail.com>
2023-08-23 15:41:16 +0300

    exercise2

--- index2.js
+++ index2.js
@@ -1,8 +1,6 @@
-
 // בנו תכנית שמדמה את המשחק אבן נייר ומספריים
 // התכנית תגדיר מי המנצח לפי חוקיות המשחק
 // אבן מנצח מספריים שמנצחות נייר שמנצח אבן
-
 // דגשים
 // שחקן  יכול לבחור בין אבן נייר ומספרים בלבד! אך שחקן יכול שלא תהיה לו בחירה
 // התכנית יכולה להחזיר את המחרוזות
@@ -13,36 +11,29 @@
 // union
 // במידה ולשחקן אין בחירה הצג זרקו שגיאה
 // ממשו את הפונקציה הוסיפו טיפוסים לפרמטרים ולערך החזרה של הפונקציה
-
-enum choeises {
-    Paper = 'paper',
-    Stone = 'stone',
-    Cut = 'cut'
-}
-
-type Player = {
-    move : choeises
-}
-
-const player1 : Player = {move : choeises.Paper}
-const player2 : Player = {move : choeises.Stone}
-
-
-function playGame (player1 : Player, player2: Player):string {
-    if (player1.move === 'stone' && player2.move === 'stone'|| 
-    player1.move === 'paper' && player2.move === 'paper'||
-    player1.move === 'cut' && player2.move === 'cut') {
-        return 'tie'
-    }else if (player1.move === 'stone' && player2.move === 'cut'|| 
-    player1.move === 'paper' && player2.move === 'stone'||
-    player1.move === 'cut' && player2.move === 'paper'){
-        return 'player1'
-    }else{
-        return 'player2'
+var choeises;
+(function (choeises) {
+    choeises["Paper"] = "paper";
+    choeises["Stone"] = "stone";
+    choeises["Cut"] = "cut";
+})(choeises || (choeises = {}));
+var player1 = { move: choeises.Paper };
+var player2 = { move: choeises.Stone };
+function playGame(player1, player2) {
+    if (player1.move === 'stone' && player2.move === 'stone' ||
+        player1.move === 'paper' && player2.move === 'paper' ||
+        player1.move === 'cut' && player2.move === 'cut') {
+        return 'tie';
+    }
+    else if (player1.move === 'stone' && player2.move === 'cut' ||
+        player1.move === 'paper' && player2.move === 'stone' ||
+        player1.move === 'cut' && player2.move === 'paper') {
+        return 'player1';
+    }
+    else {
+        return 'player2';
     }
 }
-
-const play = playGame(player1, player2);
+var play = playGame(player1, player2);
 console.log(play);
 //Output: player1 or player2 or tie
-
